a8bcf0355b2f7ea3ef56a7fbf51650e0e9012ace
Phuc Huynh <phuchh@nhsv.vn>
2025-07-26 14:46:23 +0700

    setup build env

--- src/hooks/useProductForm.ts
+++ src/hooks/useProductForm.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { apiProduct } from "../src/api/apiServer";
 import { useToastQueue } from "../src/utils/showToast";
 
